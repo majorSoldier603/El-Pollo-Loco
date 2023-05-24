8c2520ceaa1b2a0e5ce9f0701cf66d12c7532308
Schmax603 <stark.m.2004@gmail.com>
2023-05-24 11:30:21 +0200

    did background did animation

---
+++ models/chicken-object.class.js
@@ -0,0 +1,36 @@
+class Chicken extends MovableObject {
+
+
+    height = 80;
+    width = 86;
+    y = 360;
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+
+    
+    constructor() {
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 200 + Math.random() * 500;
+        this.animate();
+    }
+
+
+
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200)
+
+    }
+
+
+}
---
+++ models/cloud.class.js
@@ -0,0 +1,20 @@
+class Cloud extends MovableObject {
+    y = 20;
+    height = 250;
+    width = 500;
+
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.x = 200 + Math.random() * 500;
+        this.animate();
+
+    }
+
+    animate() {
+        this.moveLeft();
+    }
+
+
+}
