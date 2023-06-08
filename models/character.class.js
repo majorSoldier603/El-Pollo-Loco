c0081f3278922da8366104c7fd1bde01a568473c
Schmax603 <stark.m.2004@gmail.com>
2023-06-08 21:58:12 +0200

    fix level end

--- models/character.class.js
+++ models/character.class.js
@@ -31,13 +31,13 @@ class Character extends MovableObject {
         'img/2_character_pepe/5_dead/D-54.png',
         'img/2_character_pepe/5_dead/D-55.png',
         'img/2_character_pepe/5_dead/D-56.png',
-        'img/2_character_pepe/5_dead/D-57.png',
+        'img/2_character_pepe/5_dead/D-57.png'
     ]
 
     IMAGES_HURT = [
         'img/2_character_pepe/4_hurt/H-41.png',
         'img/2_character_pepe/4_hurt/H-42.png',
-        'img/2_character_pepe/4_hurt/H-43.png',
+        'img/2_character_pepe/4_hurt/H-43.png'
     ]
 
     world;
